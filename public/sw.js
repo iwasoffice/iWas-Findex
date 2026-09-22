2fcc8d834987ee20774a3ab8460eb8eb60c44bcf
Wale Iwarere <iwasofficial@outlook.com>
2026-09-23 00:48:47 +0100

    Polish mobile responsiveness and market presentation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "iwas-findex-v2";
+const CACHE = "iwas-findex-v3";
 const STATIC = ["/", "/offline", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)));
