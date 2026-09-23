cc0790c47ad47a6b341e9c43f5ad2409f14de52a
Wale Iwarere <iwasofficial@outlook.com>
2026-09-23 01:04:04 +0100

    Replace site branding with official iWas Findex logo

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Offline() {
   return (
     <main className="center-page">
-      <Image src="/brand-mark.svg" alt="iWas Findex" width={76} height={76} />
+      <Image src="/brand-mark.png" alt="iWas Findex" width={76} height={76} />
       <h1>You are offline</h1>
       <p>The installed app can reopen cached screens, but fresh market data needs an internet connection.</p>
       <Link href="/">Try again</Link>
